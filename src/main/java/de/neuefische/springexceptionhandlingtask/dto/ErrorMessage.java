8a5d34b759e2332cc80f634a730aa1ce06defa01
Carsten Kremser <github@carstenkremser.de>
2024-04-09 16:35:50 +0200

    add timestamp to ErrorMessage

--- src/main/java/de/neuefische/springexceptionhandlingtask/dto/ErrorMessage.java
+++ src/main/java/de/neuefische/springexceptionhandlingtask/dto/ErrorMessage.java
@@ -1,7 +1,14 @@
 package de.neuefische.springexceptionhandlingtask.dto;
 
+import java.time.Instant;
+
 public record ErrorMessage(
         int errorCode,
-        String message
+        String message,
+        Instant timestamp
 ) {
+
+    public ErrorMessage(int errorCode, String message) {
+        this(errorCode, message, Instant.now());
+    }
 }
